5e07a6f94b7795eb1654492383eb26c17e4ac631
chickenplace <jakeMsaperfad@gmail.com>
2026-09-03 15:29:51 -0500

    saves

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,21 @@
 import Image from "next/image";
+import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
+
+export default async function Page() {
+  const cookieStore = await cookies()
+  const supabase = createClient(cookieStore)
+
+  const { data: sales } = await supabase.from('sales table').select()
+
+  return (
+    <ul>
+      {sales?.map((sale) => (
+        <li key={sale.id}>{sale.name}</li>
+      ))}
+    </ul>
+  )
+}
 
 export default function Home() {
   return (
